9b2b1aad411032d5460574f2d5a3be71aa02d0e1
Ye Min <yehtut.it@gmail.com>
2017-04-12 16:49:18 +0800

    remove emoji btn if there is no format on toolbar

--- src/module-toolbar-emoji.js
+++ src/module-toolbar-emoji.js
@@ -8,8 +8,9 @@ class ToolbarEmoji {
             this.toolbar.addHandler('emoji', this.checkPalatteExist);
         
         var emojiBtn = document.getElementsByClassName('ql-emoji')[0];
-        emojiBtn.innerHTML = '<svg viewbox="0 0 18 18"><circle class="ql-fill" cx="7" cy="7" r="1"></circle><circle class="ql-fill" cx="11" cy="7" r="1"></circle><path class="ql-stroke" d="M7,10a2,2,0,0,0,4,0H7Z"></path><circle class="ql-stroke" cx="9" cy="9" r="6"></circle></svg>';
-        
+        if (emojiBtn) {
+            emojiBtn.innerHTML = '<svg viewbox="0 0 18 18"><circle class="ql-fill" cx="7" cy="7" r="1"></circle><circle class="ql-fill" cx="11" cy="7" r="1"></circle><path class="ql-stroke" d="M7,10a2,2,0,0,0,4,0H7Z"></path><circle class="ql-stroke" cx="9" cy="9" r="6"></circle></svg>';
+        };
     }
 
     checkPalatteExist() {
