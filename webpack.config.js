53ea295bf87484a9b09860e14286d0bb341ae60a
Klas Lundberg <klas@mindroute.com>
2018-02-26 23:22:27 +0100

    Indentation

--- webpack.config.js
+++ webpack.config.js
@@ -2,54 +2,54 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
-    entry: './src/quill-emoji.js',
-    output: {
-        filename: 'quill-emoji.js',
-        path: path.resolve(__dirname, 'dist'),
-        library: "QuillEmoji",
-        libraryTarget: "umd"
-    },
-    mode: "production",
-    externals: {
-        quill: 'Quill',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: [
-                    path.resolve(__dirname, "src/")
-                ],
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [['env', {modules: false}],]
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new UglifyJSPlugin({
-          uglifyOptions: {
-            compress: {
-                warnings: false,
-                conditionals: true,
-                unused: true,
-                comparisons: true,
-                sequences: true,
-                dead_code: true,
-                evaluate: true,
-                join_vars: true,
-                if_return: true
-            },
-            output: {
-                comments: false
-            }
+  entry: './src/quill-emoji.js',
+  output: {
+    filename: 'quill-emoji.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: "QuillEmoji",
+    libraryTarget: "umd"
+  },
+  mode: "production",
+  externals: {
+    quill: 'Quill',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, "src/")
+        ],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['env', {modules: false}],]
           }
-        })
+        }
+      }
     ]
+  },
+  plugins: [
+    new UglifyJSPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: false,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          join_vars: true,
+          if_return: true
+        },
+        output: {
+          comments: false
+        }
+      }
+    })
+  ]
 };
 
 module.exports = config;
