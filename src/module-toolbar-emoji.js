1fccfe957b8ca89c5c48fbec8e08a5f0642ec20d
Klas Lundberg <klas@mindroute.com>
2018-02-27 13:50:41 +0100

    Toolbar Icon Option

--- src/module-toolbar-emoji.js
+++ src/module-toolbar-emoji.js
@@ -17,7 +17,7 @@ class ToolbarEmoji extends Module {
     var emojiBtns = document.getElementsByClassName('ql-emoji');
     if (emojiBtns) {
       [].slice.call( emojiBtns ).forEach(function ( emojiBtn ) {
-        emojiBtn.innerHTML = '<svg viewbox="0 0 18 18"><circle class="ql-fill" cx="7" cy="7" r="1"></circle><circle class="ql-fill" cx="11" cy="7" r="1"></circle><path class="ql-stroke" d="M7,10a2,2,0,0,0,4,0H7Z"></path><circle class="ql-stroke" cx="9" cy="9" r="6"></circle></svg>';
+        emojiBtn.innerHTML = options.buttonIcon;
       });
     }
   }
@@ -33,6 +33,10 @@ class ToolbarEmoji extends Module {
     });
   }
 }
+
+ToolbarEmoji.DEFAULTS = {
+  buttonIcon: '<svg viewbox="0 0 18 18"><circle class="ql-fill" cx="7" cy="7" r="1"></circle><circle class="ql-fill" cx="11" cy="7" r="1"></circle><path class="ql-stroke" d="M7,10a2,2,0,0,0,4,0H7Z"></path><circle class="ql-stroke" cx="9" cy="9" r="6"></circle></svg>'
+};
 
 function fn_close(){
   let ele_emoji_plate = document.getElementById('emoji-palette');
