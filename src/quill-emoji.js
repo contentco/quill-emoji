0fdfe76a71bf3fa58c42a180e672ffaf9fe9efd6
Timothy Li <himynameistimli@gmail.com>
2018-09-15 16:54:07 +0800

    Update webpack, sheets, and modify imports
    Move .ap-* styles into separate emoji.scss file
    Add other _20, _32, _64px sheet sizes and default to 64.
    Instead of import Quill, check for presence of window.Quill
    Update webpack and package.json to handle scss and images
    Add angular example

--- src/quill-emoji.js
+++ src/quill-emoji.js
@@ -1,15 +1,16 @@
-import Quill from 'quill';
-
 import EmojiBlot from './format-emoji-blot';
 import ShortNameEmoji from './module-emoji';
 import ToolbarEmoji from './module-toolbar-emoji';
 import TextAreaEmoji from './module-textarea-emoji';
+import './scss/quill-emoji.scss';
 
-Quill.register({
-  'formats/emoji': EmojiBlot,
-  'modules/emoji-shortname': ShortNameEmoji,
-  'modules/emoji-toolbar': ToolbarEmoji,
-  'modules/emoji-textarea': TextAreaEmoji
-}, true);
+if (window.Quill) {
+  window.Quill.register({
+      'formats/emoji': EmojiBlot,
+      'modules/emoji-shortname': ShortNameEmoji,
+      'modules/emoji-toolbar': ToolbarEmoji,
+      'modules/emoji-textarea': TextAreaEmoji
+    }, true);  
+}
 
 export default { EmojiBlot, ShortNameEmoji, ToolbarEmoji, TextAreaEmoji };
