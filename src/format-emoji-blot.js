0967bde606588c20436da9585869380e6781f600
Klas Lundberg <klas@mindroute.com>
2018-10-02 13:07:52 +0200

    Changed emoji to span
    - Replaced img with span and unicode fallback for text extraction
    - Enter to select quickfix

--- src/format-emoji-blot.js
+++ src/format-emoji-blot.js
@@ -3,53 +3,28 @@ import Quill from 'quill';
 const Embed = Quill.import('blots/embed');
 
 class EmojiBlot extends Embed {
-    static create(value) {
-        let node = super.create();
-        if (typeof value === 'object') {
-            node.classList.add("emoji");
-            node.classList.add("ap");
-            node.classList.add("ap-"+value.name);
-            let dataUrl = 'data:image/png;base64,R0lGODlhAQABAAAAACwAAAAAAQABAAA=';
-            node.setAttribute('src',dataUrl);
-            node.setAttribute('data-resize', 'no');
-        }
-        else if(typeof value === 'string'){
-            node.setAttribute('src',value);
-        }
-        return node;
-      }
-
-    static formats(node) {
-        // We still need to report unregistered src formats
-        let format = {};
-        if (node.hasAttribute('class')) {
-          format.class = node.getAttribute('class');
-        }
-        if (node.hasAttribute('alt')) {
-          format.width = node.getAttribute('alt');
-        }
-        return format;
+  static create(value) {
+    let node = super.create();
+    if (typeof value === 'object') {
+      node.setAttribute('data-name', value.name);
+      let emojiSpan = document.createElement('span');
+      emojiSpan.classList.add(this.emojiClass);
+      emojiSpan.classList.add(this.emojiPrefix + value.name);
+      emojiSpan.innerText = String.fromCodePoint('0x' + value.unicode);
+      node.appendChild(emojiSpan);
     }
+    return node;
+  }
 
-    static value(node) {
-        return node.getAttribute('src');
-    }
-
-    format(name, value) {
-
-        if (name === 'class' || name === 'alt') {
-          if (value) {
-            this.domNode.setAttribute(name, value);
-          } else {
-            this.domNode.removeAttribute(name, value);
-          }
-        } else {
-          super.format(name, value);
-        }
-    }
+  static value(node) {
+    return node.contentText;
+  }
 }
 
 EmojiBlot.blotName = 'emoji';
-EmojiBlot.tagName = 'img';
+EmojiBlot.className = 'ql-emojiblot';
+EmojiBlot.tagName = 'span';
+EmojiBlot.emojiClass = 'ap';
+EmojiBlot.emojiPrefix = 'ap-';
 
 export default EmojiBlot;
