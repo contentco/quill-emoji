c222676d78a7e2db9c5fed3387753f47155a7887
Cameron White <cwhite@connectfirst.com>
2018-10-15 15:26:25 -0600

    - updating format-emoji-blot.js so the `create` method properly creates a node object from the value returned from the `value` method

--- src/format-emoji-blot.js
+++ src/format-emoji-blot.js
@@ -1,4 +1,5 @@
 import Quill from 'quill';
+import emojiMap from "./emoji-map";
 
 const Embed = Quill.import('blots/embed');
 
@@ -6,18 +7,30 @@ class EmojiBlot extends Embed {
   static create(value) {
     let node = super.create();
     if (typeof value === 'object') {
-      node.setAttribute('data-name', value.name);
-      let emojiSpan = document.createElement('span');
-      emojiSpan.classList.add(this.emojiClass);
-      emojiSpan.classList.add(this.emojiPrefix + value.name);
-      emojiSpan.innerText = String.fromCodePoint('0x' + value.unicode);
-      node.appendChild(emojiSpan);
+    
+      EmojiBlot.buildSpan(value, node);
+    } else if (typeof value === "string") {
+      const valueObj = emojiMap[value];
+
+      if (valueObj) {
+        EmojiBlot.buildSpan(valueObj, node);
+      }
     }
+
     return node;
   }
 
   static value(node) {
-    return node.contentText;
+    return node.dataset.name;
+  }
+
+  static buildSpan(value, node) {
+    node.setAttribute('data-name', value.name);
+    let emojiSpan = document.createElement('span');
+    emojiSpan.classList.add(this.emojiClass);
+    emojiSpan.classList.add(this.emojiPrefix + value.name);
+    emojiSpan.innerText = String.fromCodePoint('0x' + value.unicode);
+    node.appendChild(emojiSpan);
   }
 }
 
