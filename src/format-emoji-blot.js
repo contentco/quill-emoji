8b0a01d7fce79c4cd547b98343857ac3f2eb4495
gaoxueyuan <gaoxueyuan@kuaishou.com>
2019-01-14 18:17:24 +0800

    Fix emoji unicode parse error,fixes #48

--- src/format-emoji-blot.js
+++ src/format-emoji-blot.js
@@ -7,7 +7,7 @@ class EmojiBlot extends Embed {
   static create(value) {
     let node = super.create();
     if (typeof value === 'object') {
-    
+
       EmojiBlot.buildSpan(value, node);
     } else if (typeof value === "string") {
       const valueObj = emojiMap[value];
@@ -29,8 +29,12 @@ class EmojiBlot extends Embed {
     let emojiSpan = document.createElement('span');
     emojiSpan.classList.add(this.emojiClass);
     emojiSpan.classList.add(this.emojiPrefix + value.name);
-    emojiSpan.innerText = String.fromCodePoint('0x' + value.unicode);
+    // unicode can be '1f1f5-1f1ea',see emoji-list.js.
+    emojiSpan.innerText = String.fromCodePoint(...EmojiBlot.parseUnicode(value.unicode));
     node.appendChild(emojiSpan);
+  }
+  static parseUnicode(string) {
+    return string.split('-').map(str => parseInt(str, 16));
   }
 }
 
